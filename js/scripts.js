4d0ff5299327faba97dc5ffe3da0f1a9dcd3b6ee
Nina Nikoghosyan <ninain@gmail.com>
2019-02-24 13:06:26 -0800

    create survey questions in index.html and branching answers in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
+    var name=$("input#name");
     var age = parseInt($("input#age").val());
-    var gender = $("select#gender").val();
-    var hair = $("select#hair").val();
+    var attractions =$("select#attractions").val();
+    var activeStay = $("select#activeStay").val();
+
+
+
+    if (age < 60 && attractions === 'mountain' && activeStay === 'yes') {
+      $("#result").show()
+      $("#gateaway").text("skiing or hiking in the mountains. You could choose countries such as Peru, Swtitzerlad, Armenia");
+    }
+    else if (age >= 60 && attractions === 'beach' || activeStay === 'no') {
+      $("#result").show()
+      $("#gateaway").text("enjoying sunshine on white sandy beaches. Some of our suggestions are Hawaii, Mexico, Bahamas or Greece.");
+    }
+    else if (age <= 60 && attractions === 'organized' && activeStay === 'yes') {
+      $("#result").show()
+      $("#gateaway").text("going abroad and disvovering new countries and new cultures. Discover China, Thailand or Japan for ancient easter cultures. Try France, Italy or Spain for medieval western cultures." );
+    }
+    else if (attractions === 'beach' && activeStay === 'yes') {
+      $("#result").show()
+      $("#gateaway").text("surfing in Australia, snorkeling in Egypt or diving in Alaska." );
+    }
+
+
+
+    $("#result").show();
     event.preventDefault();
-
-
-    if (hair === 'fair' && gender === 'female') {
-      $("#result").show()
-      $("#celebrity").text("Madonna");
-    }
-    if (hair === 'brown' && gender === 'female') {
-      $("#result").show()
-      $("#celebrity").text("Monica Belucci")
-    }
-    if (hair === 'brown' && gender === 'male') {
-      $("#result").show()
-      $("#celebrity").text("Bradley Cooper")
-    }
-    if (hair === 'fair' && gender === 'male') {
-      $("#result").show()
-      $("#celebrity").text("Brad Pitt")
-    }
-
-    $("#result").show()
 
   });
 });
